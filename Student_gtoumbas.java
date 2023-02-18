e925ea15f36a96d587b86f8b4926e49c25d1802c
gtoumbas <70962697+gtoumbas@users.noreply.github.com>
2023-02-18 13:38:18 -0500

    gitignore and fixed variable names

--- Student_gtoumbas.java
+++ Student_gtoumbas.java
@@ -61,8 +61,8 @@ public class Student_gtoumbas implements Student {
         List<Double> synergies) {
 
         double K = averageNumStudentsAbove(N, S, T, W, aptitude, schools, synergies);
-        double k_star = (T / (T + W)) * (1 + (W / (T + W))) * K;
-        return k_star;
+        double kStar = (T / (T + W)) * (1 + (W / (T + W))) * K;
+        return kStar;
     }
 
     public int[] getApplications(
@@ -74,36 +74,36 @@ public class Student_gtoumbas implements Student {
         List<Double> schools,
         List<Double> synergies) {
           
-        School[] true_prefs = new School[schools.size()];
+        School[] truePrefs = new School[schools.size()];
         for (int i = 0; i != synergies.size(); ++i) {
-        true_prefs[i] = new School(i, schools.get(i) + synergies.get(i));
+        truePrefs[i] = new School(i, schools.get(i) + synergies.get(i));
         }
-        Arrays.sort(true_prefs);
+        Arrays.sort(truePrefs);
         int[] ret = new int[10];
 
         // Case where N = 10, return top 10 schools
         if (N == 10) {
         for (int i = 0; i != 10; ++i) {
-            ret[i] = true_prefs[i].index;
+            ret[i] = truePrefs[i].index;
         }
         return ret;
         }
 
         // Computer K*
-        double K_star = strategyScore(N, S, T, W, aptitude, schools, synergies);
+        double kStar = strategyScore(N, S, T, W, aptitude, schools, synergies);
 
         // Check if K* + 10 > N
         // If so, return last 10 schools
-        if (K_star + 10 > N) {
+        if (kStar + 10 > N) {
         for (int i = 0; i != 10; ++i) {
-            ret[i] = true_prefs[true_prefs.length - 1 - i].index;
+            ret[i] = truePrefs[truePrefs.length - 1 - i].index;
         }
         return ret;
         }
 
         // Otherwise, return K - K* + 10 schools
         for (int i = 0; i != 10; ++i) {
-        ret[i] = true_prefs[(int) K_star + i].index;
+        ret[i] = truePrefs[(int) kStar + i].index;
         }
         return ret;
     }
